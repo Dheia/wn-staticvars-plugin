86a60128152fca669de6b233a955bcf5e5c7ee04
JKVaco <brute1904@gmail.com>
2023-03-09 02:19:50 +0500

    Added partials and localize

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -3,6 +3,22 @@
 return [
     'plugin' => [
         'name'        => 'Static Vars',
-        'description' => 'No description provided yet...',
+        'description' => 'Output variables and static info',
+    ],
+
+    'statuses' => [
+        0   => 'New',
+        10  => 'Approved',
+        20  => 'Completed',
+        100 => 'Disabled',
+        110 => 'Active',
+        410 => 'Removal',
+    ],
+
+    'targets' => [
+        '_self'   => 'Current window',
+        '_blank'  => 'In new tab',
+        '_parent' => 'Frame-parent',
+        '_top'    => 'Full window',
     ],
 ];
